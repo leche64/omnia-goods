b53c7fac9e11d878cbb1decc8e9e28659d3fcd50
Leche <lechedeburro420@gmail.com>
2022-04-21 18:06:53 -0400

    send verification code works

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,8 @@ const Register = require("../models/Register");
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const jwt = require("jsonwebtoken");
-const accountSid = "AC5473c19b88688100d4d4ebd61ea45e4e"; //process.env.TWILIO_ACCOUNT_SID;
-const authToken = "348d77ed12fe42e75a5eb2ab49a070cd"; //process.env.TWILIO_AUTH_TOKEN;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioClient = require("twilio")(accountSid, authToken);
 
 router.get("/register", (req, res) => {
@@ -65,64 +65,88 @@ router.post(
   }
 );
 
-router.get("/get-registrations", (req, res) => {
+router.get("/login", (req, res) => {
   Register.find().then((result) => {
     res.send(result);
   });
 });
 
 router.get("/verify", (req, res) => {
-  twilioClient.verify.services(process.env.TWILIO_SERVICE_SSID).verificationChecks.create({
-    to: '+15714387789',
-    code: "1111",
-  }).then((result) =>{
-    if(result.status == 'approved') {
-      console.log('successfully verified code');
+  twilioClient.verify
+    .services(process.env.TWILIO_SERVICE_SSID)
+    .verificationChecks.create({
+      to: "+15714387789",
+      code: "1111",
+    })
+    .then((result) => {
+      if (result.status == "approved") {
+        console.log("successfully verified code");
+      }
+      res.status(200).send({ result });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(200).send({ error });
+    });
+});
+
+router.post(
+  "/login",
+  urlencodedParser,
+  [check("mobilenumber", "isMobilePhone Validation Failed").isMobilePhone()],
+  async (req, res) => {
+    const error = validationResult(req);
+
+    if (!error.isEmpty()) {
+      console.log(error);
+      return res.status(422).json({
+        success: false,
+        errors: error.array(),
+      });
+    } else {
+      // validate input number
+      console.log(req.body.mobilenumber);
+      // trim white space
+      const phoneNumber = req.body.mobilenumber.replace(/\s/g, "");
+
+      const phoneNumberTrim = phoneNumber.replace(/-/g, "");
+
+      const phoneNumberFinal = "+1" + phoneNumberTrim;
+
+      console.log(phoneNumberFinal);
+
+      // check if phone number exist in db
+      const existingPhoneNumber = await Register.find({
+        mobilePhoneNumber: phoneNumberTrim,
+      }).exec();
+      console.log(existingPhoneNumber);
+
+      // non registered mobile phone number, send to registration
+      if (existingPhoneNumber.length == 0) {
+        console.log("New Mobile Number Needs To Be Registered");
+      } else {
+        console.log("Mobile Number Already Registered and Verified");
+        
+        // if already existed, send twilio verification
+        console.log("SENDING TWILIO VERIFCATION");
+        // twilioClient.verify
+        //   .services(process.env.TWILIO_SERVICE_SSID)
+        //   .verifications.create({
+        //     to: phoneNumberFinal,
+        //     channel: "sms",
+        //   })
+        //   .then((result) => {
+        //     console.log("successfully sent twilio verification code");
+        //     res.status(200).send({ result });
+        //   })
+        //   .catch((error) => {
+        //     console.log(error);
+        //     res.status(200).send({ error });
+        //   });
+        res.render("verify");
+      }
     }
-    res.status(200).send({result})
-  }).catch((error) =>{
-    console.log(error);
-    res.status(200).send({error});
-  });
-});
-
-router.post("/login", async (req, res) => {
-  //validiate input params
-
-  //check if phone number exists
-
-  //if exists, validate via twilio
-  twilioClient.verify.services(process.env.TWILIO_SERVICE_SSID).verifications.create({
-    to: '+15714387789',
-    channel: "sms",
-  }).then((result) =>{
-    console.log('successfully sent twilio verification code');
-    res.status(200).send({result})
-  }).catch((error) =>{
-    console.log(error);
-    res.status(200).send({error});
-  });
-
-  // console.log("twilio start");
-  // client.messages
-  //   .create({
-  //     body: "Did you just order Cambodian breast milk??",
-  //     from: "+19803032992",
-  //     to: "+15714387789",
-  //   })
-  //   .then((message) => console.log(message.sid));
-
-  // console.log("twilio end");
-
-  //if twililo verficatioon pass, create and assing jwt
-  // const token = jwt.sign(
-  //   {
-  //     _id: user.id,
-  //   },
-  //   process.env.TOKEN_SECERT
-  // );
-
-  // res.header('auth-token', token).send(token);
-});
+  }
+);
 
 module.exports = router;
