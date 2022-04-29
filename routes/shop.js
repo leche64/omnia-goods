0638f02b538ae23d3de34edb2d90b2ec57b7469b
Leche <lechedeburro420@gmail.com>
2022-04-28 21:35:01 -0400

    dev

--- routes/shop.js
+++ routes/shop.js
@@ -33,61 +33,120 @@ router.post("/order", async (req, res) => {
   console.log(req.body.edible);
   console.log(req.body.vape);
 
-  var flowerText = null;
-  var edibleText = null;
-  var vapeText = null;
+  var flowerDescription = null;
+  var flowerPrice = null;
+  var flowerWeight = null;
 
   if (req.body.card != null) {
     if (req.body.card == "card_one") {
-      flowerText = "Ronin $60";
+      flowerDescription = "Ronin";
+      flowerPrice = "60.00";
+      flowerWeight = "3.5 grams";
     } else if (req.body.card == "card_two") {
-      flowerText = "Daimyos $110";
+      flowerDescription = "Daimyos";
+      flowerPrice = "110.00";
+      flowerWeight = "7 grams";
     } else if (req.body.card == "card_three") {
-      flowerText = "Samurai $200";
+      flowerDescription = "Samurai";
+      flowerPrice = "200.00";
+      flowerWeight = "14 grams";
     } else if (req.body.card == "card_four") {
-      flowerText = "Shogun $360";
+      flowerDescription = "Shogun";
+      flowerPrice = "360";
+      flowerWeight = "28 grams";
     }
   }
+
+  const flowerOrder = {
+    description: flowerDescription,
+    price: flowerPrice,
+    weight: flowerWeight,
+  };
+  console.log(flowerOrder);
 
   if (req.body.edible != null) {
+    var edibleDescription = null;
+    var ediblePrice = null;
+    var edibleWeight = null;
+
     if (req.body.edible == "edible_one") {
-      edibleText = "Choc Blaze Bar $20";
+      edibleDescription = "Choc Blaze Bar";
+      ediblePrice = "20.00";
+      edibleWeight = "150 mg";
     } else if (req.body.edible == "edible_two") {
-      edibleText = "Gummy Snack Pack $60";
+      edibleDescription = "Choc Blaze Bar";
+      ediblePrice = "60.00";
+      edibleWeight = "500 mg";
     }
   }
+
+  const edibleOrder = {
+    description: edibleDescription,
+    price: ediblePrice,
+    weight: edibleWeight,
+  };
+  console.log(edibleOrder);
 
   if (req.body.vape != null) {
+    var vapeDescription = null;
+    var vapePrice = null;
+    var vapeWeight = null;
+
     if (req.body.vape == "vape_one") {
-      vapeText = "Pre-Roll $20";
+      vapeText = "Pre-Roll";
+      vapePrice = "20.00";
+      vapeWeight = "1 g";
     } else if (req.body.vape == "vape_two") {
-      vapeText = "Doobies Pack (5) $50";
+      vapeText = "Doobies Pack (5)";
+      vapePrice = "50.00";
+      vapeWeight = "3.5 g";
     } else if (req.body.vape == "vape_three") {
-      vapeText = "Dirt Cart $60";
+      vapeText = "Vape Cart Pack";
+      vapePrice = "60.00";
+      vapeWeight = "1 g";
     }
   }
 
-  console.log(flowerText);
-  console.log(edibleText);
-  console.log(vapeText);
+  const vapeOrder = {
+    description: vapeText,
+    price: vapePrice,
+    weight: vapeWeight,
+  };
 
-  res.redirect("/api/shop/order");
+  console.log(vapeOrder);
 
-  const messageBody =
-    "\n\n Your order from Omnia Goods has been recieved. Our courier will deliver the products to you later today. \n\n" +
-    flowerText +
-    "\n" +
-    edibleText +
-    "\n" +
-    vapeText;
+  const orderTotal = parseInt(flowerOrder.price) + parseInt(edibleOrder.price) + parseInt(vapeOrder.price);
 
-  twilioClient.messages
-    .create({
-      body: messageBody,
-      from: "+19803032992",
-      to: "+15714387789",
-    })
-    .then((message) => console.log(message.sid));
+  const tax = 0.05;
+  const deliveryFee = 10;
+
+  const totalTax = orderTotal * tax;
+
+  res.render("cart", {
+    flower: flowerOrder,
+    edible: edibleOrder,
+    vape: vapeOrder,
+    subTotal: orderTotal.toFixed(2),
+    taxTotal: parseFloat(totalTax).toFixed(2),
+    deliveryTotal: deliveryFee.toFixed(2),
+    orderTotal: parseFloat(orderTotal + totalTax + deliveryFee).toFixed(2)
+  });
+
+  // const messageBody =
+  //   "\n\n Your order from Omnia Goods has been recieved. Our courier will deliver the products to you later today. \n\n" +
+  //   flowerText +
+  //   "\n" +
+  //   edibleText +
+  //   "\n" +
+  //   vapeText;
+
+  // twilioClient.messages
+  //   .create({
+  //     body: messageBody,
+  //     from: "+19803032992",
+  //     to: "+15714387789",
+  //   })
+  //   .then((message) => console.log(message.sid));
 });
 
 module.exports = router;
